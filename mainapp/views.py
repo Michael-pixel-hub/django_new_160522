a81e01c8cd23486edc9200db49d13fb767110da0
michael <kurashevmichael@gmail.com>
2022-05-23 17:03:16 +0300

    add lesson2

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,13 +1,23 @@
 from django.shortcuts import render
+from geekshop.settings import BASE_DIR
+import json
 
 
 def index(request):
-    return render(request, "mainapp/index.html")
+    context = {
+        'title': 'Магазин',
+    }
+    return render(request, "mainapp/index.html", context)
 
 
 def products(request):
-    return render(request, "mainapp/products.html")
+    with open(BASE_DIR/"mainapp/json/content.json", 'r', encoding='utf-8') as file:
+        context = json.load(file)
+    return render(request, "mainapp/products.html", context)
 
 
 def contact(request):
-    return render(request, "mainapp/contact.html")
+    context = {
+        'title': 'Контакты'
+    }
+    return render(request, "mainapp/contact.html", context)
